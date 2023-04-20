46348b79ea3a5dd0431aecda79b7d53608c29999
zzuuckk <102153314+ZzuuCkk@users.noreply.github.com>
2023-04-20 13:17:55 +0400

    finished 3 task

---
+++ app.js
@@ -0,0 +1,31 @@
+// N1
+function powNumber(a ,b){
+    return (Math.pow(a,b));
+}
+
+console.log(powNumber(2,3));
+
+// N2
+function priceSale(price=1000,percent=10){
+    return price * percent /100;
+    // console.log(sale);
+}
+
+console.log(priceSale());
+
+// N3
+function randomNumber(a){
+    return Math.floor( Math.random(a) * 100 );
+    
+}
+console.log(randomNumber(100));
+
+// N4
+
+function lengthOfString(str =""){
+    let strLength = lengthOfString.length
+ return strLength;
+}
+console.log(lengthOfString("zura"))
+
+
